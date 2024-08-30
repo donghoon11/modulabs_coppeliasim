e3e4ad33a77b1d9ad19d7d12ae6cd3e8755637b4
donghoon11 <hoon6049@hanyang.ac.kr>
2024-08-30 14:08:48 +0900

    new file:   wall_follower/maze_solver_exercise.ttt 	modified:   wall_follower/wall_follow.py

--- wall_follower/wall_follow.py
+++ wall_follower/wall_follow.py
@@ -1,15 +1,22 @@
 # zmq remote api 로 변환하기.
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+
 class LumiBot:
     def __init__(self):
-        self.lmotor = self.get_object_handle("./lumibot_leftMotor")
-        self.rmotor = self.get_object_handle("./lumibot_rightMotor")
+        self.client = RemoteAPIClient()
+        self.sim = self.client.require('sim')
+
+    def sysCall_init(self):
+        self.lmotor = self.sim.getObjectHandle("./lumibot_leftMotor")
+        self.rmotor = self.sim.getObjectHandle("./lumibot_rightMotor")
         
-        self.sensorLF = self.get_object_handle('./LeftFront')
-        self.sensorLR = self.get_object_handle('./LeftRear')
-        self.sensorRF = self.get_object_handle('./RightFront')
-        self.sensorRR = self.get_object_handle('./RightRear')
-        self.sensorF = self.get_object_handle('./Forward')
+        self.sensorLF = self.sim.getObjectHandle('./LeftFront')
+        self.sensorLR = self.sim.getObjectHandle('./LeftRear')
+        self.sensorRF = self.sim.getObjectHandle('./RightFront')
+        self.sensorRR = self.sim.getObjectHandle('./RightRear')
+        self.sensorF = self.sim.getObjectHandle('./Forward')
         
+        # params for driving
         self.min_d = 0.15
         self.max_d = 0.25
         self.yaw_cutoff = 0.005
@@ -26,48 +33,48 @@ class LumiBot:
         self.diff = 0
         self.fwd = self.fwd_default
     
-    def get_object_handle(self, name):
-        # Placeholder for the actual API call
-        return sim.getObjectHandle(name)
+    # def get_object_handle(self, name):
+    #     # Placeholder for the actual API call
+    #     return sim.getObjectHandle(name)
 
-    def set_joint_target_velocity(self, handle, velocity):
-        # Placeholder for the actual API call
-        sim.setJointTargetVelocity(handle, velocity)
+    # def set_joint_target_velocity(self, handle, velocity):
+    #     # Placeholder for the actual API call
+    #     return self.sim.setJointTargetVelocity(handle, velocity)
     
-    def read_proximity_sensor(self, sensor):
-        # Placeholder for the actual API call
-        return sim.readProximitySensor(sensor)
+    # def read_proximity_sensor(self, sensor):
+    #     # Placeholder for the actual API call
+    #     return self.sim.readProximitySensor(sensor)
     
-    def actuation(self):
-        self.set_joint_target_velocity(self.lmotor, self.v_straight)
-        self.set_joint_target_velocity(self.rmotor, self.v_straight)
+    def sysCall_actuation(self):
+        self.sim.setJointTargetVelocity(self.lmotor, self.v_straight)
+        self.sim.setJointTargetVelocity(self.rmotor, self.v_straight)
         
         if self.fwd < self.fwd_cutoff:
             print('going toward the wall, turn right')
-            self.set_joint_target_velocity(self.lmotor, self.v_sharp)
-            self.set_joint_target_velocity(self.rmotor, 0)
+            self.sim.setJointTargetVelocity(self.lmotor, self.v_sharp)
+            self.sim.setJointTargetVelocity(self.rmotor, 0)
         elif self.fwd > self.fwd_cutoff:
             if self.avg > self.max_d:
                 print('going away from the wall, turn left')
-                self.set_joint_target_velocity(self.lmotor, self.v - self.dv)
-                self.set_joint_target_velocity(self.rmotor, self.v)
+                self.sim.setJointTargetVelocity(self.lmotor, self.v - self.dv)
+                self.sim.setJointTargetVelocity(self.rmotor, self.v)
             elif self.avg < self.min_d:
                 print('going toward the wall, turn right')
-                self.set_joint_target_velocity(self.lmotor, self.v)
-                self.set_joint_target_velocity(self.rmotor, self.v - self.dv)
+                self.sim.setJointTargetVelocity(self.lmotor, self.v)
+                self.sim.setJointTargetVelocity(self.rmotor, self.v - self.dv)
             elif self.min_d < self.avg < self.max_d:
                 if self.diff > self.yaw_cutoff:  # LF > LR
                     print('yaw correction: turn left')
-                    self.set_joint_target_velocity(self.lmotor, self.v - self.dv)
-                    self.set_joint_target_velocity(self.rmotor, self.v)
+                    self.sim.setJointTargetVelocity(self.lmotor, self.v - self.dv)
+                    self.sim.setJointTargetVelocity(self.rmotor, self.v)
                 elif self.diff < -self.yaw_cutoff:  # LF < LR
-                    self.set_joint_target_velocity(self.lmotor, self.v)
-                    self.set_joint_target_velocity(self.rmotor, self.v - self.dv)
+                    self.sim.setJointTargetVelocity(self.lmotor, self.v)
+                    self.sim.setJointTargetVelocity(self.rmotor, self.v - self.dv)
     
-    def sensing(self):
-        flag1, LF = self.read_proximity_sensor(self.sensorLF)
-        flag2, LR = self.read_proximity_sensor(self.sensorLR)
-        flag3, F = self.read_proximity_sensor(self.sensorF)
+    def sysCall_sensing(self):
+        flag1, LF = self.sim.readProximitySensor(self.sensorLF)
+        flag2, LR = self.sim.readProximitySensor(self.sensorLR)
+        flag3, F = self.sim.readProximitySensor(self.sensorF)
         
         if flag1 == 0 and flag2 == 1:
             self.avg = LR
@@ -92,7 +99,13 @@ class LumiBot:
     def cleanup(self):
         pass
 
-# # Example usage
-# lumibot = LumiBot()
-# lumibot.sensing()
-# lumibot.actuation()
+    def run_coppelia(self):
+        self.sim.startSimulation()
+        while True:
+            self.sysCall_actuation()
+            self.sysCall_sensing()
+
+if __name__ == "__main__" :
+    client = LumiBot()
+    client.sysCall_init()
+    client.run_coppelia()
