680f154d1b464c17bce29d00997d632600243ab9
donghoon11 <hoon6049@hanyang.ac.kr>
2024-09-18 02:14:21 +0900

    modified:   localization/__pycache__/youBot.cpython-310.pyc 	modified:   localization/grid_mapping.py

--- localization/grid_mapping.py
+++ localization/grid_mapping.py
@@ -44,12 +44,11 @@ class MappingBot(YouBot):
         loc = self.read_ref()
         
         # update grid
-        self.grid.update(loc, scan) # ?
+        self.grid.update(loc, scan)
 
 class Grid():
     def __init__(self):
         # grid 전체 배열 크기 100*100 으로 설정.
-        # test 환경 : 5m * 5m
         self.grid = np.zeros((100, 100))
         # plot grid
         r = np.linspace(-5, 5, 101)  # -5부터 5까지 101개의 x 좌표 생성
@@ -63,7 +62,7 @@ class Grid():
         self.scan_theta = np.array([- np.pi / 2 + self.delta * i for i in range(13)])
         self.boundary = np.pi / 2 + self.delta / 2      
         # min distance
-        self.min_dist = (2 * (0.05**2)) ** 0.5
+        self.min_dist = (2 * (0.05**2)) ** 0.5  # 0.07071
 
     def update(self, loc, scan):
         self.mapping(loc, scan)
@@ -71,7 +70,7 @@ class Grid():
         self.visualize(loc, scan)
 
     def mapping(self, loc, scan):
-        x, y, theta = loc
+        x, y, theta = loc       # youBot_ref (x,y,yaw)
         # scan position
         # 라이다 위치는 ref position (로봇 중앙) 으로부터 앞으로 0.275 m 으로 조정.
         rx = x + 0.275 * np.cos(theta)
@@ -80,30 +79,34 @@ class Grid():
         dist = 2.25     # 스캔 센서 감지 최대 거리
         '''
         i_min, i_max, j_min, j_max는 그리드 맵에서 스캔 범위 내의 인덱스를 결정. 
-        그리드의 해상도는 0.1 미터이고, 그리드 맵이 100 x 100 크기를 가지며, 맵의 중앙이 (0, 0)에 해당
+        그리드의 해상도는 0.1 미터이고, 그리드 맵이 100 x 100 크기를 가지며, 맵의 중앙이 (50,50)에 해당
         '''
+        # xy_resolution = 0.1
         i_min = max(0, int((rx - dist) // 0.1 + 50))
         i_max = min(99, int((rx + dist) // 0.1 + 50))
         j_min = max(0, int((ry - dist) // 0.1 + 50))
         j_max = min(99, int((ry + dist) // 0.1 + 50))
         # sub grid
-        sub_grid = self.grid[j_min : j_max + 1, i_min : i_max + 1]
+        sub_grid = self.grid[j_min : j_max + 1, i_min : i_max + 1]  # grid[row, col] = grid[y_range, x_range]
 
         # x distance
-        gx = np.arange(i_min, i_max +1) * 0.1 + 0.05 -8
-        gx = np.repeat(gx.reshape(1,-1), sub_grid.shape[0], axis=0)
-        dx = gx - rx
+        # 1) i_min 부터 i_max 까지 정수 배열 생성 -> 0.1 해상도 곱하여 미터 단위 변환
+        # 2) 배열 원소에 +0.05 함으로써 각 그리드 셀의 중심 좌표 구함.
+        # 3) 그리드 크기가 10*10 이므로 0으로부터 상대적 중앙을 구하기 위해 -5
+        gx = np.arange(i_min, i_max +1) * 0.1 + 0.05 - 5     # numpy.arange([start, ] stop, [step, ] dtype=None)
+        gx = np.repeat(gx.reshape(1,-1), sub_grid.shape[0], axis=0)     # gx 배열을 1*N 형태로 변환.
+        dx = gx - rx    # 라이다 위치로부터 sub grid 각 셀의 x 방향 거리 추정.
         # y distance
         gy = np.arange(j_min, j_max + 1) * 0.1 + 0.05 - 5
         gy = np.repeat(gy.reshape(1,-1).T, sub_grid.shape[1], axis=1)
         dy = gy - ry
         # distance
-        gd = (dx**2 + dy**2) ** 0.5
+        gd = (dx**2 + dy**2) ** 0.5     # 라이다 위치로부터 sub grid 각 셀 까지의 거리 추정.
 
         # theta diff
         # 각 지점과 로봇의 스캔 세서 사이의 각도 차이를 구하고, 로봇이 바라보는 방향 theta 와 비교해 각도 차이 dtheta 를 계산.
-        gtheta = np.arccos(dx / gd) * ((dy > 0) * 2 -1)
-        dtheta = gtheta - theta
+        gtheta = np.arccos(dx / gd) * ((dy > 0) * 2 -1)     # gtheta : global coord.origin 에 대한 광선의 각도
+        dtheta = gtheta - theta                             # dtheta : 로봇의 yaw 를 고려한 광선의 상대 각도. 로봇 이동 방향 기준으로 계산된 광선의 각도.
         # 각도 범위 조정. (-pi ~ pi)
         while np.pi < np.max(dtheta):
             dtheta -= (np.pi < dtheta) * 2 * np.pi
@@ -111,21 +114,28 @@ class Grid():
             dtheta += (dtheta < -np.pi) * 2 * np.pi
         
         # inverse sensor model
-        # scan 에는 총 13개의 스캔 데이터가 들어 있다.
+        '''
+        Return values of 'sim.readProximitySensor'
+        :   res = detection state (0 or 1)
+            dist = distance to th detected point
+            point = array of 3 numbers indicating the relative coord. of the detected point
+            obj = handle of the object that was detected
+            n = normal vector of the detected surface relative to the snesor reference frame. 
+        '''
         # res 는 센서가 장애물을 감지했는지 여부, dist 는 장애물과의 거리
         for i in range(13):
             res, dist, _, _, _ = scan[i]
-            if res == 0:        # 장애물이 없는 경우 -> 해당 방향에서 특정 거리 내에 있는 셀들을 자유 공간으로 간주.
+            if res == 0:        # 장애물이 없는 경우 -> 해당 방향에서 특정 거리 내에 있는 셀들을 자유 공간으로 간주. free area
                 area = (
-                    (gd <= 2.25)    # 그리드 상의 각 지점과 로봇 센서 사이의 거리가 2.25 미터 이하인지 확인.
+                    (gd <= 2.25)    # sub gird 각 셀과 로봇 센서 사이의 거리가 2.25 미터 이하인지 확인.
                     * (-self.boundary + self.delta * i <= dtheta)       # i 번째 스캔 데이터가 커버하는 각도 범위 내에 있는 그리드 셀만을 업데이트 대상으로.
                     * (dtheta <= -self.boundary + self.delta * (i +1))
                 )
                 sub_grid[area] -= 0.5   # 위 조건을 만족하는 area 에서 그리드 셀의 값을 0.5 만큼 감소 -> 자유공간 가능성.
             else:       # 장애물 감지.
-                dist = min(2.25, dist)
+                dist = min(2.25, dist)      # dist 재정의.
                 detect_area = (
-                    (np.abs(gd - dist) < self.min_dist)     # 그리드 상의 각 지점과 로봇 센서 사이의 거리가 탐지된 거리와 매우 근접한지 여부 호가인.
+                    (np.abs(gd - dist) < self.min_dist)     # 그리드 상의 각 지점과 로봇 센서 사이의 거리가 탐지된 거리와 매우 근접한지 여부 확인. 기준 값 : 0.07071
                     * (-self.boundary + self.delta * i <= dtheta)
                     * (dtheta <= -self.boundary + self.delta * (i + 1))
                 )
