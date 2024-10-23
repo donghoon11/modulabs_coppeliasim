eb49dcb43053f06207659567d59d264773827ed9
donghoon11 <hoon6049@hanyang.ac.kr>
2024-10-23 19:06:44 +0900

    modified:   planning/youBot.ttt 	modified:   planning/youBotPP.py 	deleted:    planning_tracking/mpc.py

--- planning/youBotPP.py
+++ planning/youBotPP.py
@@ -2,14 +2,11 @@ from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 import math
 import numpy as np
 import time
-# from youBot_controll import MecanumWheelController
 
-
-class MobileRobotPP:
-    def __init__(self):
+class youBotPP:
+    def __init__(self) -> None:
         self.client = RemoteAPIClient()
         self.sim = self.client.require('sim')
-
         self.simOMPL = self.client.require('simOMPL')
         self.run_flag = True
         self.not_first_here = False
@@ -18,27 +15,31 @@ class MobileRobotPP:
         self.robotHandle = self.sim.getObject('/youBot')
         self.refHandle = self.sim.getObject('/youBot_ref')
         self.frontRefHandle = self.sim.getObject('/youBot_frontRef')
-
-        # KUKA youBot 은 4 mecanum wheel 기반으로 differential drive 기반의 로봇과는 차이가 있다.
-        # self.leftMotorHandle = self.sim.getObject('/rollingJoint_fl')
-        # self.rightMotorHandle = self.sim.getObject('/rollingJoint_fr')
-        self.wheel_joints = [
-            self.sim.getObject('/rollingJoint_fl'),     # wheel_joints[0]
-            self.sim.getObject('/rollingJoint_rl'),     # wheel_joints[1]
-            self.sim.getObject('/rollingJoint_rr'),     # wheel_joints[2]
-            self.sim.getObject('/rollingJoint_fr')      # wheel_joints[3]
-        ]
-
-        self.collVolumeHandle = self.sim.getObject('/youBot_coll')     # dr12 의 boundary box
+        self.collVolumeHandle = self.sim.getObject('/youBot_coll')
         self.goalDummyHandle = self.sim.getObject('/youBot_goalDummy')
 
-        self.robotObstaclesCollection = self.sim.createCollection(0)        # 충돌 검사용 객체 컬렉션
-        self.sim.addItemToCollection(self.robotObstaclesCollection, self.sim.handle_all, -1, 0)
+        self.motor_fl = self.sim.getObject('/rollingJoint_fl')
+        self.motor_rl = self.sim.getObject('/rollingJoint_rl')
+        self.motor_fr = self.sim.getObject('/rollingJoint_fr')
+        self.motor_rr = self.sim.getObject('/rollingJoint_rr')
+
+        '''
+        sim.createCollection()
+        int options : bit 0 set (1): collection overrides collidable, measurable, detectable properties, and also the visibility state of its objects.
+        Return : collectionHandle: handle of the new collection
+        
+        sim.addItemToCollection(int collectionHandle, int what, int objectHandle, int options)
+        int options :
+            bit 0 set (1): the specified object (or group of objects) is removed from the collection. Otherwise it is added.
+            bit 1 set (2): the specified object is not included in the group of objects, if sim.handle_tree or sim.handle_chain is specified (i.e. the tree base or tip is excluded).
+        '''
+        self.robotObstaclesCollection = self.sim.createCollection(0)
+        self.sim.addItemToCollection(self.robotObstaclesCollection, self.sim.handle_all, -1, 0)     # -1 : 마지막 object = cuboid(벽)
         self.sim.addItemToCollection(self.robotObstaclesCollection, self.sim.handle_tree, self.robotHandle, 1)
         self.collPairs = [self.collVolumeHandle, self.robotObstaclesCollection]
 
         self.velocity = 180 * math.pi / 180
-        self.search_range = 5
+        self.search_range = 10
         self.search_algo = self.simOMPL.Algorithm.BiTRRT
         self.search_duration = 0.1
         self.display_collision_free_nodes = True
@@ -46,146 +47,157 @@ class MobileRobotPP:
         self.show_track_pos = True
         self.line_container = None
 
-
-    # 충돌 검사 메서드
     def check_collides_at(self, pos):
         tmp = self.sim.getObjectPosition(self.collVolumeHandle, -1)
-        self.sim.setObjectPosition(self.collVolumeHandle, -1, pos)
 
-        # 충돌 쌍을 사용하여 충돌 여부를 확인 -> return bool
+        # 특정 pos 로 이동한 다음 충돌 검사.
+        self.sim.setObjectPosition(self.collVolumeHandle, pos, -1)
         collision = self.sim.checkCollision(self.collPairs[0], self.collPairs[1])
-        self.sim.setObjectPosition(self.collVolumeHandle, -1, tmp)
-        return collision
+        print(collision)        # collision[0] 결과가 0이면 충돌.
 
-    # 목표 더미 객체의 위치를 반환한다.
+        # 다시 원래위치로 옮겨오기.
+        self.sim.setObjectPosition(self.collVolumeHandle, tmp, -1)
+        symbol = True if collision[0] == 0 else False
+        return symbol
+    
     def get_target_position(self):
-        """Returns the position of the goal dummy object."""
         return self.sim.getObjectPosition(self.goalDummyHandle, -1)
     
-    
     def visualize_path(self, path):
-        """Visualizes the robot's path."""
-        if not self.line_container:     # 초기
-            self.line_container = self.sim.addDrawingObject(self.sim.drawing_lines, 3, 0, -1, 99999, [0.2, 0.2, 0.2])
-
-        self.sim.addDrawingObject(self.line_container, None)
+        '''
+        sim.addDrawingObject(int objectType, float size, float duplicateTolerance, 
+                            int parentObjectHandle, int maxItemCount, list color = None)
+        '''
+        if self.line_container is None:
+            self.line_container = self.sim.addDrawingObject(self.sim.drawing_lines, 3, 0, -1, 99999, [0.2,0.2,0.2])
+        self.sim.addDrawingObjectItem(self.line_container, None)
 
         if path:
-            for i in range(1, len(path)/2):
-                # 현재 포인트와 이전 포인트를 연결하는 라인 데이터를 생성.
+            for i in range(1, len(path) // 2):
                 line_data = [path[2*i], path[2*i+1], 0.001, path[2*i-2], path[2*i-1], 0.001]
                 self.sim.addDrawingObjectItem(self.line_container, line_data)
 
-
-    def move_robot_to_position(self, target_position):
-        path = None     # 경로 초기화
+    def move_robot_to_position(self, target_position, path=None):
+        
         while not path:
-            task = self.simOMPL.createTask('t')     # 새로운 경로 생성.
-            self.simOMPL.setAlgorithm(task, self.search_algo)
-
-            start_pos = self.sim.getObjectPosition(self.refHandle, -1)  # 로봇의 현재 위치
+            task = self.simOMPL.createTask('t')
             
-            # 상태 공간 생성 : 2d 공간에서 로봇이 탐색할 상태 공간을 생성한다.
-            # 상태 공간의 경계를 설정한다ㅣ.
+            # path planning 수행하는 과정
+            self.simOMPL.setAlgorithm(task, self.search_algo)
+            start_pos = self.sim.getObjectPosition(self.robotHandle, -1)
+            
+            '''
+            string stateSpaceHandle = simOMPL.createStateSpace(string name, int type, int objectHandle, 
+                                                                list boundsLow, list boundsHigh, int useForProjection, 
+                                                                float weight=1.0, int refObjectHandle=-1)
+            '''
             ss = [self.simOMPL.createStateSpace('2d', self.simOMPL.StateSpaceType.position2d, self.collVolumeHandle,
-                                                [start_pos[0]-self.search_range, start_pos[1]-self.search_range],
-                                                [start_pos[0]+self.search_range, start_pos[1]+self.search_range], 1)]
+                                                [start_pos[0] - self.search_range, start_pos[1] - self.search_range],
+                                                [start_pos[0] + self.search_range, start_pos[1] + self.search_range], 1)]
             self.simOMPL.setStateSpace(task, ss)
             self.simOMPL.setCollisionPairs(task, self.collPairs)
             self.simOMPL.setStartState(task, start_pos[:2])
             self.simOMPL.setGoalState(task, target_position[:2])
-            
-            # 유효성 검사 해상도 설정.
-            self.simOMPL.setStateValidityCheckingResolution(task, 0.001)
+            self.simOMPL.setStateValidityCheckingResolution(task, 0.01)
             self.simOMPL.setup(task)
-
+            print('path planning is done')
+            # 경로 단순화
             if self.simOMPL.solve(task, self.search_duration):
-                # 경로 단순화
                 self.simOMPL.simplifyPath(task, self.search_duration)
                 path = self.simOMPL.getPath(task)
+                print(f'path : {path}')
                 self.visualize_path(path)
 
-            time.sleep(0.01)    # 경로 생성 과정에서 잠시 대기.
+            # time.sleep(0.01)
         return path
     
-    
+    def omni_wheel_control(self, v_forward, v_side, v_turn):
+        # params for 4 mecanum wheel drive
+        radius = 0.05       # wheel radius
+        dist_R = 0.228 + 0.158      # (distance b.w. centroid & wheel cent.) = dist_x + dist_y
+
+        # Calculate wheel velocities for mecanum drive
+        fl_speed = (-v_forward - v_side - v_turn * dist_R ) / radius
+        rl_speed = (-v_forward + v_side - v_turn * dist_R ) / radius
+        fr_speed = (-v_forward + v_side + v_turn * dist_R) / radius
+        rr_speed = (-v_forward - v_side + v_turn * dist_R ) / radius
+
+        # print(f"fl_spped : {fl_speed}")
+        # print(f"rl_spped : {rl_speed}")
+        # print(f"fr_spped : {fr_speed}")
+        # print(f"rr_spped : {rr_speed}")
+        # print()
+
+        # Set motor velocities
+        self.sim.setJointTargetVelocity(self.motor_fl, fl_speed)
+        self.sim.setJointTargetVelocity(self.motor_rl, rl_speed)
+        self.sim.setJointTargetVelocity(self.motor_fr, fr_speed)
+        self.sim.setJointTargetVelocity(self.motor_rr, rr_speed)
+
     def follow_path(self, path):
         if path:
-            path_3d = []    # 경로를 3d 좌표로 변환할 리스트 초기화 (*이 부분도 np.로 최적화할 수 있지 않을까 생각.)
-            # 경로를 2d 포인트에서 3d 포인트로 변환하기 위해 반복.
-            for i in range(0, len(path)//2):
-                path_3d.extend([path[2*i], path[2*i+1], 0.0])       # 각 2d 포인트에 z좌표 0을 추가하여 3d 좌표로 변환.
+            path_3d = []
+            for i in range(0, len(path)//2) :
+                path_3d.extend([path[2*i], path[2*i+1], 0.0])       # 3차원 좌표로 수정
 
-            prev_l = 0  # 이전 경로 위치 초기화
+            prev_l = 0
             track_pos_container = self.sim.addDrawingObject(self.sim.drawing_spherepoints | self.sim.drawing_cyclic, 0.02, 0, -1, 1, [1, 0, 1])
             while True:
                 current_pos = self.sim.getObjectPosition(self.frontRefHandle, -1)
-                path_lengths, total_dist = self.sim.getPathLenghts(path_3d, 3)
-                
-                # 현재 위치와 가장 가까운 경로의 위치를 찾는다.
-                closet_l = self.sim.getClosetPosOnPath(path_3d, path_lengths, current_pos)
-                
-                # 이전 위치보다 가까운 경우를 체크.
-                if closet_l <= prev_l:
-                    # 경로를 부드럽게 하기 위해 최소 거리 조정.
-                    closet_l += total_dist / 200
-                prev_l = closet_l   # 위치 업데이트
+                path_length, total_dist = self.sim.getPathLengths(path_3d, 3)   # list path, int dof
 
-                # 가장 가까운 위치에 대한 목표 지점을 보간한다.
-                target_point = self.sim.getPathInterpolatedConfig(path_3d, path_lengths, closet_l)
+                closet_l = self.sim.getClosestPosOnPath(path_3d, path_length, current_pos)
+
+                if closet_l <= prev_l:
+                    closet_l += total_dist / 200
+                prev_l = closet_l
+
+                target_point = self.sim.getPathInterpolatedConfig(path_3d, path_length, closet_l)
                 self.sim.addDrawingObjectItem(track_pos_container, target_point)
 
-                # Relative position of the target position
                 m = self.sim.getObjectMatrix(self.refHandle, -1)
-                self.sim.inverseMatrix(m)
+                self.sim.getMatrixInverse(m)
+                
                 relative_target = self.sim.multiplyVector(m, target_point)
 
-                # Compute angle
                 angle = math.atan2(relative_target[1], relative_target[0])
 
-                # Adjust wheel velocities
-                left_v = 1.0 - 4 * angle / math.pi if angle > 0 else 1.0
-                right_v = 1.0 + 4 * angle / math.pi if angle <= 0 else 1.0
-                # 속도가 -1보다 낮아지지 않도록 제한.
-                left_v = max(-1.0, left_v)
-                right_v = max(-1.0, right_v)
+                forward_vel = 8.0
+                turn_vel = 4 * angle / math.pi
+                side_vel = 0.0
 
-                self.sim.setJointTargetVelocity(self.leftMotorHandle, left_v * self.velocity)
-                self.sim.setJointTargetVelocity(self.rightMotorHandle, right_v * self.velocity)
+                self.omni_wheel_control(forward_vel, side_vel, turn_vel)
 
-                # Break when close to the target when the distance becomes 0.05
-                if np.linalg.norm(np.array(self.sim.getObjectPosition(self.goalDummyHandle, -1)) - 
-                                  np.array(self.sim.getObjectPosition(self.refHandle, -1))) < 0.05:
+                if np.linalg.norm(np.array(self.sim.getObjectPosition(self.goalDummyHandle, -1)) -
+                                  np.array(self.sim.getObjectPosition(self.refHandle, -1))) < 0.4:
                     break
-
-                time.sleep(0.01)        # 매 루프 사이에 잠시 대기.
-
+                
+                # time.sleep(0.01)
 
     def run_step(self):
-        # self.sim.setStepping(True)
         self.sim.startSimulation()
-        count = 0
         while self.run_flag:
             goal_position = self.get_target_position()
-            
-            # Adjust goal if collision is detected:
             while self.check_collides_at(goal_position):
+                print('1')
+                print(f'goal position : {goal_position}')
+                # 충돌 보정
                 goal_position[0] -= 0.09
-
-            # Plan and follow the path
+                goal_position[1] -= 0.09
+            print('2')
             path = self.move_robot_to_position(goal_position)
+            print('3')
             if path:
+                print('4')
                 self.follow_path(path)
-
-            # Stop
-            self.sim.setJointTargetVelocity(self.leftMotorHandle, 0.0)
-            self.sim.setJointTargetVelocity(self.rightMotorHandle, 0.0)
-
+            
             time.sleep(0.01)
+            print('5')
+            self.omni_wheel_control(0.0,0.0,0.0)
+            break
         self.sim.stopSimulation()
 
-
 if __name__ == "__main__":
-    controller = MobileRobotPP()
+    controller = youBotPP()
     controller.init_coppelia()
     controller.run_step()
